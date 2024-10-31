1b21003f642e24ca4e219e6e1da4450a415655a3
javeedsocial <78414554+javeedsocial@users.noreply.github.com>
2024-10-31 18:02:10 +0530

    use template

--- main.tf
+++ main.tf
@@ -2,7 +2,11 @@ module "ec2_instance" {
   source              = "git::https://github.com/jvsocial/terraform-aws-free-tier-ec2.git"
   security_group_name = var.security_group_name
   instance_name       = var.instance_name
-  user_data           = file("${path.module}/user_data.sh")
+  #user_data           = file("user_data.sh")
+  user_data = templatefile("user_data.tpl", {
+    hostname = var.instance_name
+  })
+
 
 
 }
